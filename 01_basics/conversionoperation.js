37f26656195820e3cdcc9f653698a000c9317542
salman89481 <112274941+salman89481@users.noreply.github.com>
2024-01-03 02:28:06 +0000

    conversion of data types

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,23 @@
+let score = "33abcc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" = > 33
+//"33abc" = > Nan
+//true = 1
+//false =0
+
+let value = ""
+let BoolValue = Boolean(value)
+console.log(typeof BoolValue);
+console.log(BoolValue);
+
+// 1 => true
+//"" => false
+//"salman" => true
