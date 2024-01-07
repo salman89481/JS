6785b56368cca26ef9ec45e6f64d646769b0fb0f
salman89481 <112274941+salman89481@users.noreply.github.com>
2024-01-07 02:22:40 +0000

    math and nmber in javascript

---
+++ 01_basics/nums_math.js
@@ -0,0 +1,16 @@
+//const num = 10
+//const variable = new Number(100)
+//console.log(num);
+//console.log(variable);
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
