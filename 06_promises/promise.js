aa62c9d4f48c5d6b1807ebacbff48a07a9899cde
salman89481 <112274941+salman89481@users.noreply.github.com>
2024-02-21 06:40:08 +0000

    oops learned

--- 06_promises/promise.js
+++ 06_promises/promise.js
@@ -1,45 +1,45 @@
-const promiseOne = new Promise(function(resolve , reject){
-    //Do an async task
-    //DB calls , cryptography,networking
-    setTimeout(function(){
-        console.log("Async task is complete");
-        resolve();
-    },1000)
-});
+// const promiseOne = new Promise(function(resolve , reject){
+//     //Do an async task
+//     //DB calls , cryptography,networking
+//     setTimeout(function(){
+//         console.log("Async task is complete");
+//         resolve();
+//     },1000)
+// });
 
-promiseOne.then(function(){
-    console.log("Promise Consumed");
-})
-
-
-
-new Promise(function(resolve ,  reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("async code resolved");
-})
+// promiseOne.then(function(){
+//     console.log("Promise Consumed");
+// })
 
 
 
-const promiseThree = new Promise(function(resolve , reject){
-    setTimeout(function(){
-        resolve({username:"salman",email:"salman94157@example.com"})
-    },1000);
-});
+// new Promise(function(resolve ,  reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     },5000)
+// }).then(function(){
+//     console.log("async code resolved");
+// })
 
-promiseThree.then(function(user){
-    console.log(user);
-})
+
+
+// const promiseThree = new Promise(function(resolve , reject){
+//     setTimeout(function(){
+//         resolve({username:"salman",email:"salman94157@example.com"})
+//     },1000);
+// });
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// })
 
 
 
 
 const promiseFour = new Promise(function(resolve,reject){
     setTimeout(function(){
-        let error = false;
+        let error = true;
         if (!error) {
             resolve({username : "ahmad" , email : "salman8948@gamil.in"});
             
@@ -62,3 +62,78 @@ promiseFour.then((user)=>{
 })
 
 
+// const promiseFive =  new Promise(function(resolve , reject){
+//     setTimeout(function(){
+//         let error = false
+//         if (!error){
+//             resolve({username : "javascript", password:"abc"})
+//         }else{
+//             reject("Error :JS went wrong")
+//         }
+//     },1000)
+// })
+
+// async function ConsumePromiseFive(){
+//     try{
+//         const response = await promiseFive()
+//         console.log(response);
+
+//     } catch(error){
+//         console.log(error);
+//     }
+// }
+
+//  ConsumePromiseFive()
+
+// function myFun(result) {
+//     console.log(result);
+// }
+
+// console.log('Start');
+// getData(myFun);
+// console.log('End');
+
+// function getData(callback) {
+//     setTimeout(function() {
+//         callback('Data fetched asynchronously');
+//     }, 2000);
+// }
+
+
+// async function getAllUsers(){
+    //     try{
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    //         const data  =await response.json();
+    //         console.log(data);
+    
+    //     } catch (error){
+        //         console.log("E:", error);
+        //     }
+        // }
+
+        // getAllUsers();
+        
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then((response)=>{
+//     return response.json()
+// }).then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=> console.log(error))
+// .finally()
+
+
+
+// function myFun(result) {
+//         console.log(result);
+//     }
+    
+//     console.log('Start');
+//     getData(myFun);
+//     console.log('End');
+    
+//     function getData(callback) {
+//         setTimeout(function() {
+//             callback('Data fetched asynchronously');
+//         }, 2000);
+//     }
---
+++ 07_classes_and_oops/notes.md
@@ -0,0 +1,23 @@
+#Javascript and Classes
+
+##OOps
+
+#object
+-collection of properties and methods
+-toLowerCase
+
+#Why we use oops
+
+#parts of oop
+object literal 
+
+-constructor function
+-prototypes
+-classes
+-instances (new , this)
+
+##4pillars
+abstraction
+encapsulation 
+inheritance
+polymorphism
